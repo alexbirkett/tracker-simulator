b88b89bc4b87541360c55485d80abf4c76b6d7ef
Alex Birkett <alex@birkett.co.uk>
2012-09-09 11:55:08 +0200

    spawn a new connection every 5ms

--- goTopSimulator.js
+++ goTopSimulator.js
@@ -6,17 +6,38 @@ var MESSAGE_CMD_T = "#861785001515349,CMD-T,V,DATE:120903,TIME:160649,LAT:59.932
 var MESSAGE_CMD_X = "#861785001515349,CMD-X#";
 var MESSAGE_ALM_A = "#861785001515349,ALM-A,V,DATE:120903,TIME:160649,LAT:59.9326566N,LOT:010.7875033E,Speed:005.5,X-X-X-X-82-10,000,24202-0324-0E26#";
 
-
 var connect = function(callback) {
-	trackerSimulator([MESSAGE_CMD_T, MESSAGE_CMD_X, MESSAGE_ALM_A], 0, 0, callback);
+	trackerSimulator([ MESSAGE_CMD_T, MESSAGE_CMD_X, MESSAGE_ALM_A ], 1000, 100,
+			callback);
 };
 
-var i = 0;
+var openConnections = 0;
+var totalConnections = 0;
 
-var test = function() {
-	return (i++ < 100);
+var spawnConnections = function() {
+
+	console.log('hello');
+	
+	var i = 0;
+	var doSpawn = function() {
+		openConnections++;
+		totalConnections++;
+		connect(function() {
+			openConnections--;
+		});
+		i++;
+		if (i < 30000) {
+		setTimeout(doSpawn, 5);
+		}
+	}
+	process.nextTick(doSpawn);
+
+};
+var showDebug = function() {
+	var closedConnections = totalConnections - openConnections;
+	console.log('open conneciton ' + openConnections + ' total connections '
+			+ totalConnections + ' closed connections ' + closedConnections);
 };
 
-async.whilst(test, connect, function() {
-	console.log('done');
-});
+setInterval(showDebug, 1000);
+spawnConnections();
