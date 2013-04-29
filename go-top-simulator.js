d26418776f6a0ec32126c01e2d9a1cf37a0b62b5
Alex Birkett <alex@birkett.co.uk>
2013-04-29 14:09:42 +0200

    Fix go-top-simulator to work with latest tracker-simulator api

--- go-top-simulator.js
+++ go-top-simulator.js
@@ -1,7 +1,7 @@
 var net = require('net');
 var async = require('async');
 
-var trackerSimulator = require('./tracker-simulator');
+var TrackerSimulator = require('./tracker-simulator');
 var MESSAGE_CMD_T = "#861785001515349,CMD-T,V,DATE:120903,TIME:160649,LAT:59.9326566N,LOT:010.7875033E,Speed:005.5,X-X-X-X-49-5,000,24202-0ED9-D93B#";
 var MESSAGE_CMD_X = "#861785001515349,CMD-X#";
 var MESSAGE_ALM_A = "#861785001515349,ALM-A,V,DATE:120903,TIME:160649,LAT:59.9326566N,LOT:010.7875033E,Speed:005.5,X-X-X-X-82-10,000,24202-0324-0E26#";
@@ -28,40 +28,45 @@ var totalConnections = 0;
 var pendingConnections = 0;
 var failedConnections = 0;
 
-
-
 var connect = function(localAddress, callback) {
+	console.log('connect');
 	var connectOptions = new Object();
 	connectOptions.port = parameters.port;
 	connectOptions.host = parameters.hostname;
 	connectOptions.localAddress = localAddress;
-		
-	trackerSimulator([ MESSAGE_CMD_T, MESSAGE_CMD_X, MESSAGE_ALM_A ], 30000, 100,
-			 connectOptions, callback);
+
+    var trackerSimulator = new TrackerSimulator();
+    
+    trackerSimulator.connect(connectOptions, function(err) {
+        callback(err, trackerSimulator);
+    });		 
 };
 
 var spawnConnections = function(localAddress) {
 	var i = 0;
-
-	var handleCallback = function(event) {
-		if (event.name == "connected") {
+    
+	var connectCallback = function(err, trackerSimulator) {
+		if (err) {
+		    pendingConnections--;
+            failedConnections++;
+		} else {
 			openConnections++;
-			pendingConnections--;
-			doConnect();
-		} else if (event.name == "error") {
-			pendingConnections--;
-			failedConnections++;
-			console.log(event.err);
+            pendingConnections--;
+            async.forever(function(callback) {
+                trackerSimulator.sendMessage([ MESSAGE_CMD_T, MESSAGE_CMD_X, MESSAGE_ALM_A ], 3000, 100, 100, callback);
+            }, function() {
+                openConnections--;
+            });
 		}
 	};
 	
 	var doConnect = function() {
-		if (i < 30000) {
-			connect(localAddress, handleCallback);
+		if (i < 1) {
+			connect(localAddress, connectCallback);
 			pendingConnections++;
+			i++;
 		}
 	};
-	
 	doConnect();
 };
 var showDebug = function() {
